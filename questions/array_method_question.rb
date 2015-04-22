2dc14a03710c12ea83d11a7ec8120559ba88cd53
kha <kha.les.email@gmail.com>
2015-04-21 23:45:45 -0700

    Updated Questions

--- questions/array_method_question.rb
+++ questions/array_method_question.rb
@@ -1,5 +1,12 @@
 class Array
   define_method(:trim) do
-    self.shift().pop()
+    self.shift()
+    self.pop()
+    self
   end
 end
+
+
+
+#why do i get #<Proc:0x007fe7d08ff088@(irb):175 (lambda)> instead of :trim
+#and what does it even mean?
